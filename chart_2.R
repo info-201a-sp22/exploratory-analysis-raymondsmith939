3c448da2b3e6df97902b0dbccc426ff634b4c945
angelinaali <yuchel9@uw.edu>
2022-05-11 16:50:01 -0700

    Chart 2

--- chart_2.R
+++ chart_2.R
@@ -1,17 +1,13 @@
-library(readr)
+library("readr")
 library("dplyr")
 library("ggplot2")
 us_disaster_declarations <- read_csv("Desktop/info201/us_disaster_declarations.csv")
 
 WA_disaster <- us_disaster_declarations %>% 
-  filter(state == 'WA') %>% 
-  filter()
+  filter(state == 'WA') 
 
-
-tenyears <- WA_disaster$incident_begin_date[WA_disaster$incident_begin_date <= "2022-04-05" & WA_disaster$incident_begin_date > "2012-04-05" ]
 
 ggplot (WA_disaster, aes(incident_begin_date)) +
   geom_histogram() +  
-  labs(title = "Number of Disasters in Washington by year", x = "Year", y = "Number of diasters") +
-  scale_x_continuous(breaks = seq(2012, 2022))
-
+  labs(title = "Number of Disasters in Washington by year", x = "Year", y = "Number of diasters") 
+ 
