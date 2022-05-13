7f3bb28a021fa0124390eb79c9cc34130ef8b93c
angelinaali <yuchel9@uw.edu>
2022-05-13 13:51:23 -0700

    Add chart 2

--- chart_2.R
+++ chart_2.R
@@ -11,3 +11,7 @@ ggplot (WA_disaster, aes(incident_begin_date)) +
   geom_histogram() +  
   labs(title = "Number of Disasters in Washington by year", x = "Year", y = "Number of diasters") 
  
+disasters_each_year <- WA_disaster %>% group_by(fy_declared) %>% summarise(number_disasters = n())
+
+pct_change <- max(disasters_each_year$number_disasters) / min(disasters_each_year$number_disasters)
+pct_change 
