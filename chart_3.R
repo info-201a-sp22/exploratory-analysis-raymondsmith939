51765399b44d144ec67b8d4f21c3261c8ac3306d
ajeto12 <ajeto@uw.edu>
2022-05-14 16:20:22 -0700

    Debugged_Map

--- chart_3.R
+++ chart_3.R
@@ -3,46 +3,40 @@ library("dplyr")
 library("ggplot2")
 library("maps")
 library("mapdata")
+library("scales")
+library("plotly")
 
 States <- map_data('state')
 disaster_data <- read.csv("us_disaster_declarations.csv")
 state_abbreviations <- read.csv("abbreviations.csv")
 
-## 2000 DATA  
-
-# Filter Disasters in 2000
-dis_by_st_2000 <- disaster_data %>% 
-  group_by(state) %>% 
-  filter(fy_declared == 2000)
-
 # Frequency of Disasters in 2000
-frequency_2000 <- dis_by_st_2000 %>% 
-  group_by(disaster_number) %>% 
-  mutate(frequency_2000 = n())
 
-
-## 2020 DATA
-  
-# Filter for 2020 Disasters
-dis_by_st_2020 <- disaster_data %>% 
-  group_by(state) %>% 
-  filter(fy_declared == 2020)
+freq_2000_disasters <- disaster_data %>% 
+  group_by(state, disaster_number) %>% 
+  filter(fy_declared == 2000) %>% 
+  mutate(freq_2000 = n())
 
 # Frequency of Disasters in 2020
-frequency_2020 <- dis_by_st_2020 %>% 
-  group_by(disaster_number) %>% 
-  mutate(frequency_2020 = n())
 
-## Combining and Comparing
+freq_2020_disasters <- disaster_data %>% 
+  group_by(state, disaster_number) %>% 
+  filter(fy_declared == 2020) %>% 
+  mutate(freq_2020 = n())
 
 
 # Combine the 2000 and 2020 data into a single dataframe
-freq_2000_vs_2020 <- left_join(frequency_2020, frequency_2000, by = 'state')
+freq_2000_vs_2020 <- left_join(freq_2020_disasters, freq_2000_disasters, by = 'state')
 
-# Create new variables: Change in Disasters, % Change in Disasters
+# Create new variables: Change in Disasters, Change in Disasters
 freq_2000_vs_2020 <- freq_2000_vs_2020 %>% 
-  mutate(net_change = (frequency_2020 - frequency_2000)) %>% 
-  mutate(pct_change = (net_change / frequency_2020)*100)
+  mutate(net_change = (freq_2020 - freq_2000)) # %>% 
+
+
+# Unique States
+test_freq_2000_vs_2020 <- freq_2000_vs_2020 %>% 
+  distinct(state, net_change) %>% 
+  select(state, net_change) 
 
 # Renaming columns to left_join later
 colnames(state_abbreviations) <- c('region', 'abbrev', 'state')
@@ -52,27 +46,23 @@ state_abbreviations$region = tolower(state_abbreviations$region)
 state_code <- left_join(States, state_abbreviations, by = 'region')
 
 # The data for the graph! States template + frequency data
-graph_df3 <- left_join(state_code, freq_2000_vs_2020, by = 'state')
+graph_df3 <- left_join(state_code, test_freq_2000_vs_2020, by = 'state')
 
 # The Map!
-library(scales)
 
 percent_change_map <- ggplot(States) + 
   geom_polygon(data = graph_df3,
-               aes(x = long, y = lat, group=group, fill = pct_change),
-               color = "white", size = 0) + 
-  labs(title = "Increase in Natural Disasters",
-       subtitle = 'by %, 2000-2020',
-       x = "Longitude",
-       y = "Latitude") +
-  scale_fill_gradientn(colors = c("white", "goldenrod2", "darkred"), values = rescale(c(0, 50, 100))
-                       )
+               aes(x = long, y = lat, group=group, fill = net_change),
+               color = "white") + 
+  labs(title = "Increase in Natural Disasters by Frequency",
+       x = " ",
+       y = " ",
+       fill = "Increase in # of Disasters") +
+  scale_fill_gradientn(colors = c("white", "goldenrod2", "darkred"))
+                       
 
 # Interactive Map
-library(plotly)
 ggplotly(percent_change_map)
 
 # Which U.S. state experienced the greatest increase in natural disasters since 2000?
-largest_pct_increase <- graph_df3 %>% 
-  filter(pct_change == max(pct_change)) %>% 
-  pull(region)
+# Maine increased by 438 occurrences.
